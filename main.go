b718d3ecb9835e8e65a0a261f63a56757c49fc70
unknown <chenweimi.me@gmail.com>
2020-06-28 10:35:53 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"data": "hello world"})
+	})
+
+	r.Run()
+
+	fmt.Println()
+}
